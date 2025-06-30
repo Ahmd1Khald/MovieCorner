39381fabaeaa4e456aeb5f1138e8fee879645cab
Ahmed Khaled <ahmed.khaled.sayed2000@gmail.com>
2025-06-30 19:47:18 +0300

    Make navbar component and use it in home

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './pages/home/home';
 import { MovieCard } from './shared/components/movie-card/movie-card';
+import { Navbar } from './shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [Home],
+  imports: [Home,Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule,RouterModule,FormsModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
-export class Navbar {
+export class Navbar implements OnInit {
+    constructor(private router:Router){}
 
+    ngOnInit(): void {}
+
+//   onSearch(query: string) {
+//   const trimmed = query.trim();
+//   if (trimmed.length > 0) {
+//     this.router.navigate(['/search', trimmed]);
+//   }
+//   else {
+//     this.router.navigate(['/']);
+//   }
+// }
 }
