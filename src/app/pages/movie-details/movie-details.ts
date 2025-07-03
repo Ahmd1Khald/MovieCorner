e6ee33414b6b5eebd1153f1872441fe582037538
Ahmed Khaled <ahmed.khaled.sayed2000@gmail.com>
2025-07-03 14:20:11 +0300

    Handle details errors

--- src/app/pages/movie-details/movie-details.ts
+++ src/app/pages/movie-details/movie-details.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MovieService } from '../../core/services/movie-service';
 import { WishlistService } from '../../core/services/wishlist';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Subscription } from 'rxjs';
-import { MovieVideos } from '../../movie-videos/movie-videos'; // استيراد الكومبوننت
+import { MovieVideos } from '../../movie-videos/movie-videos';
 
 @Component({
   selector: 'app-movie-details',
@@ -16,47 +16,72 @@ import { MovieVideos } from '../../movie-videos/movie-videos'; // استيراد
 })
 export class MovieDetailsComponent implements OnInit, OnDestroy {
   movie: any;
+  movieId!: number;
+  lang: string = 'en';
+  isFavorite: boolean = false;
+  isLoading: boolean = true;
+  isLoadingRecommendations: boolean = false;
+  recommendations: any[] = [];
+  showTrailer: boolean = false;
+
+  // Subscriptions
+  private routeSub!: Subscription;
+  private detailsSub!: Subscription;
+  private transSub!: Subscription;
+
   constructor(
     private route: ActivatedRoute,
     private movieService: MovieService,
-    private wishlistService: WishlistService
+    private wishlistService: WishlistService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
+    this.routeSub = this.route.params.subscribe(params => {
+      this.movieId = +params['id'];
+      this.getMovieDetails(this.movieId);
+    });
+  }
+
+  getMovieDetails(id: number): void {
+    this.isLoading = true;
+
+    this.detailsSub = this.movieService.getMovieDetails(id).subscribe({
+      next: (data: any) => {
         this.movie = data;
-
-        // بعدها نجيب الترجمات
-        if (this.transSub) this.transSub.unsubscribe();
-        this.transSub = this.movieService.getMovieTranslations(id).subscribe({
-          next: (translationsData: any) => {
-            const translation = translationsData.translations.find(
-              (t: any) => t.iso_639_1 === lang
-            );
-
-            if (translation) {
-              this.movie.translation = translation.data;
-            } else {
-              this.movie.translation = null;
-            }
-
-            this.isLoading = false;
-            this.checkIfFavorite();
-          },
-          error: () => {
-            this.movie.translation = null;
-            this.isLoading = false;
-            this.checkIfFavorite();
-          }
-        });
+        this.getMovieTranslations(id, this.lang);
       },
       error: () => {
         this.isLoading = false;
+        console.error('Error fetching movie details');
+      }
+    });
+  }
+
+  getMovieTranslations(id: number, lang: string): void {
+    if (this.transSub) this.transSub.unsubscribe();
+
+    this.transSub = this.movieService.getMovieTranslations(id).subscribe({
+      next: (translationsData: any) => {
+        const translation = translationsData.translations.find(
+          (t: any) => t.iso_639_1 === lang
+        );
+
+        this.movie.translation = translation ? translation.data : null;
+        this.isLoading = false;
+        this.checkIfFavorite();
+      },
+      error: () => {
+        this.movie.translation = null;
+        this.isLoading = false;
+        this.checkIfFavorite();
       }
     });
   }
 
   loadRecommendations(): void {
     this.isLoadingRecommendations = true;
+
     this.movieService.getRecommendations(this.movieId).subscribe({
       next: (data) => {
         this.recommendations = data.results || [];
@@ -78,10 +103,17 @@ export class MovieDetailsComponent implements OnInit, OnDestroy {
     this.isFavorite = this.wishlistService.isInWishlist(this.movie.id);
   }
 
+  navigateToMovie(movieId: number): void {
+    this.router.navigate(['/movie', movieId]);
+  }
+
+  addRecommendationToFavorites(movie: any): void {
+    this.wishlistService.toggle(movie);
+  }
+
   ngOnDestroy(): void {
-    this.langSub?.unsubscribe();
+    this.routeSub?.unsubscribe();
     this.transSub?.unsubscribe();
     this.detailsSub?.unsubscribe();
   }
-
 }
